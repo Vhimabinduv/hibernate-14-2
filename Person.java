6ac2d7ce6fb42e083d4298a707069a7bcdc3d387
Vhimabinduv <60208896+Vhimabinduv@users.noreply.github.com>
2020-02-14 04:06:15 -0800

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,41 +1,56 @@
-package com.iiht.bt2.HibernateApp1;
+package com.vp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "student")
+@Table(name="PERSON")
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Person {
 	@Id
 	@GeneratedValue
-	@Column(name="sid")	
-    int pid;
+	@Column(name="PERSON_ID")
+    private Long personId;	
+
+	@Column(name="FIRSTNAME")
+	private String firstname;
 	
-	@Column(name="name")
-    String pname;
-    
-	@Column(name="age")
-    int age;
-public int getPid() {
-	return pid;
-}
-public void setPid(int pid) {
-	this.pid = pid;
-}
-public String getPnane() {
-	return pname;
-}
-public void setPnane(String pnane) {
-	this.pname = pnane;
-}
-public int getAge() {
-	return age;
-}
-public void setAge(int age) {
-	this.age = age;
-}
-   
+	@Column(name="LASTNAME")
+	private String lastname;
+	
+	public Person() {
+	}
+	public Person(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+	
+	public Long getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(Long personId) {
+		this.personId = personId;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 }
